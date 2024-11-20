31b4331eb1e750c99df3ffdd0f67204971be097c
Sven Null <mail@sasmith.ca>
2024-11-20 14:32:52 -0400

    ahh

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { parse } from "./parser.js";
 import { tokenize } from "./tokenizer.js";
 
-const test = "r10d20kh2dh1" 
+const test = "r10d20kh2dl1" 
 const t = tokenize(test)
 console.log(test)
 console.log(JSON.stringify(t))
